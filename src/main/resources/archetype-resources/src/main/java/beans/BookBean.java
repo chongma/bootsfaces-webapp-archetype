27c02d35ba190e461363b09f054180896f117150
Matthew Broadhead <nbmlaw@gmail.com>
2018-09-13 13:00:38 +0200

    change presentation to beans

--- src/main/resources/archetype-resources/src/main/java/beans/BookBean.java
+++ src/main/resources/archetype-resources/src/main/java/beans/BookBean.java
@@ -14,7 +14,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package ${package}.presentation;
+package ${package}.beans;
 
 import ${package}.dao.BookDao;
 import ${package}.entities.Book;
